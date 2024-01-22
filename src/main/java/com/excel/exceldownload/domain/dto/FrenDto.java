da57726cbd84ca0df93904a3f3505049f174cdea
jobmania <xzxz7003@naver.com>
2024-01-23 00:30:46 +0900

    jpa 적용해버리기

--- src/main/java/com/excel/exceldownload/domain/dto/FrenDto.java
+++ src/main/java/com/excel/exceldownload/domain/dto/FrenDto.java
@@ -4,11 +4,19 @@ import com.lannstark.DefaultHeaderStyle;
 import com.lannstark.ExcelColumn;
 import com.lannstark.ExcelColumnStyle;
 import com.lannstark.style.DefaultExcelCellStyle;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.Immutable;
 
 @DefaultHeaderStyle(
         style = @ExcelColumnStyle(excelCellStyleClass = DefaultExcelCellStyle.class, enumName = "BLUE_HEADER")
 )
+@Entity
+@Immutable // 해당 Entity에 매핑되는 것은 View이기 때문에 조회만 하고 수정은 존재하지 않는다.
 public class FrenDto {
+
+    @Id
+    private Long id;
     @ExcelColumn(headerName = "가맹점명")
     private String name;
     @ExcelColumn(headerName = "주문수")
--- src/main/java/com/excel/exceldownload/repository/EduRepository.java
+++ src/main/java/com/excel/exceldownload/repository/EduRepository.java
@@ -2,16 +2,16 @@ package com.excel.exceldownload.repository;
 
 import com.excel.exceldownload.domain.dto.FrenDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class EduRepository {
-    private final DataMapper mapper;
-    public List<FrenDto> getFrenData(){
-        List<FrenDto> frenData = mapper.findFrenData();
-        return frenData;
-    }
+public interface EduRepository extends JpaRepository<FrenDto, Long> {
+
+    @Query(value = "select * from view_table", nativeQuery = true)
+    public List<FrenDto> getFrenData();
 }
